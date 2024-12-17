f81445f5a308b1826e3e533089c6231a3d81e968
Arindam Garain <107203176+garain-arindam@users.noreply.github.com>
2024-12-17 10:42:29 +0000

    updated funstions

--- systems_programming/myParserSICXE.cpp
+++ systems_programming/myParserSICXE.cpp
@@ -17,54 +17,105 @@ string decToBinary(int dec)
         reverse(binary.begin(), binary.end());
         return binary;
     }
+
+// finding the instruction format 
+int getInstFormat(unsigned int inst){
     
-string getAddressModes(string nixbpe){
-    
-    if (nixbpe.substr(0,2) = '11' || ){
-        
+    if ( inst <= 0xff) {
+        return 1;
     }
-    
+    else if ( inst <= 0xffff) {
+        return 2;
+    }
+    else if (inst <= 0xffffff){
+        return 3;
+    }
+    else if (inst <= 0xffffffff){
+        return 4;
+    }
 }
 
-string getnixbpe( unsigned int hexval){
+// validity of the instruction
+bool isValidInst(unsigned int inst){
+     switch (getInstFormat(inst))
+    {
+    case 1 :
+        return ( (inst & 0x03) == 0 );
+        break;
+    case 2 :
+        return ( (inst & 0x0300)  == 0 );
+        break;
+    case 3 :
+        return ( (inst & 0x030000)  == 0 );
+
+        break;    
+    case 4 :
+        return ( (inst & 0x03000000)  == 0 );
+
+        break;
+    default:
+        break;
+    }
+}
+
+// getting OPCODE
+string getOpCode(unsigned int inst){
+    string opCode;
+    int opcodeval;
+   
+    unsigned int opcodeval;
+
+    return opCode;
+}
+
+
+
+
+
+
+string getAddressModes(string inst){
+    string addrModes; 
+    return addrModes;
+}
+
+string getnixbpe( unsigned int inst){
+    unsigned int instfrmt = 0xffff;
     unsigned int limit = 0XFFFFFF;
-    unsigned int nixbpe = hexval & 0x03F000;
+    unsigned int nixbpe = inst & 0x03F000;
     
-    if ( hexval <= limit){
-        nixbpe = hexval & 0x03F000;
+    if ( inst <= instfrmt){
+        return "No nixbpe bits";
+    }
+
+    if ( inst <= limit){
+        nixbpe = inst & 0x03F000;
         nixbpe = nixbpe >> 12 ;
     }
     else {
-        nixbpe = hexval & 0x3F00000;
+        nixbpe = inst & 0x3F00000;
         nixbpe = nixbpe >> 20;
-        cout << "nixbpe value"<< ) << endl ;
     }
 
     return decToBinary(nixbpe);
 }
 
-unsigned int getTargetAddress(unsigned int hexvalue)}{
+unsigned int getTargetAddress(unsigned int inst){
     unsigned int ta;
     
     return ta;
 }
 
-string getInstFormat(unsigned int hexvalue){
-    string instFrmt;
 
-    return instFrmt;
-}
 
 int main() {
     // Write C++ code here
-    unsigned int hexval;
+    unsigned int inst;
    
     unsigned int nixbpe; 
 
     cout << "Enter Hex: ";
-    cin >> hex >> hexval; 
+    cin >> hex >> inst; 
+    cout << "nixbpe: " << getnixbpe(inst) << endl;
     
-    
-
     return 0;
 }
